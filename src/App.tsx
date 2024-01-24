e3fdf9d4ce7da6c82841cb0d27960dcd7f249fb2
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-01-25 00:02:00 +0700

    chores: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import { 
+import {
     Refine,
-    GitHubBanner, 
+    GitHubBanner,
     WelcomePage,
-    Authenticated, 
+    Authenticated,
 } from '@refinedev/core';
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { AuthPage,ErrorComponent
-,useNotificationProvider
-,ThemedLayoutV2
-,ThemedSiderV2} from '@refinedev/antd';
+import {
+    AuthPage, ErrorComponent
+    , useNotificationProvider
+    , ThemedLayoutV2
+    , ThemedSiderV2
+} from '@refinedev/antd';
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, { GraphQLClient, liveProvider } from "@refinedev/nestjs-query";
+import { GraphQLClient } from "@refinedev/nestjs-query";
 import { createClient } from "graphql-ws";
 import { App as AntdApp } from "antd"
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
 import { ColorModeContextProvider } from "./contexts/color-mode";
-import { Header } from "./components/header";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
 import { ForgotPassword } from "./pages/forgotPassword";
-import { authProvider } from "./authProvider";
+import { authProvider, dataProvider, liveProvider } from "./providers";
 
 const API_URL = "https://api.nestjs-query.refine.dev/graphql";
 const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
@@ -31,50 +32,36 @@ const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
 const gqlClient = new GraphQLClient(API_URL);
 const wsClient = createClient({ url: WS_URL });
 
-
-
-
 function App() {
-    
-
-    
-    
     return (
         <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            <ColorModeContextProvider>
-<AntdApp>
-            <DevtoolsProvider>
-                <Refine dataProvider={dataProvider(gqlClient)}
-liveProvider={liveProvider(wsClient)}
-notificationProvider={useNotificationProvider}
-routerProvider={routerBindings}
-authProvider={authProvider} 
-                    options={{
-                        syncWithLocation: true,
-                        warnWhenUnsavedChanges: true,
-                        useNewQueryKeys: true,
-                            projectId: "3l5diz-JqCCKT-lNNZmo",
-                        liveMode: "auto",
-                    }}
+            <GitHubBanner />
+            <RefineKbarProvider>
+                <Refine
+                    notificationProvider={useNotificationProvider}
+                    routerProvider={routerBindings}
+                    dataProvider={dataProvider}
+                    liveProvider={liveProvider}
+                    authProvider={authProvider}
+                    options={
+                        {
+                            syncWithLocation: true,
+                            warnWhenUnsavedChanges: true,
+                            useNewQueryKeys: true,
+                            projectId: "admin-b2b",
+                            liveMode: "auto"
+                        }
+                    }
                 >
-
-
-                        <Routes>
-                            <Route index element={<WelcomePage />} />
-                        </Routes>
+                    <Routes>
+                        <Route index element={<WelcomePage />}></Route>
+                    </Routes>
                     <RefineKbar />
                     <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
                 </Refine>
-            <DevtoolsPanel />
-            </DevtoolsProvider>
-            </AntdApp>
-</ColorModeContextProvider>
-        </RefineKbarProvider>
+            </RefineKbarProvider>
         </BrowserRouter>
-      );
-};
+    );
+}
 
 export default App;
